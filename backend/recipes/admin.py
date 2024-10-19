3cbd77be022f1c7cd64db2381fe0a9ee7e0d7b6f
Elizaveta <me@sunnsses.ru>
2024-10-19 13:00:10 +0300

    admin settings

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from .models import Tag, FavoriteRecipe, Ingredient, Recipe, RecipeIngredients
+from .models import FavoriteRecipe, Ingredient, Recipe, RecipeIngredients, Tag
 
 admin.site.register(Tag)
-admin.site.register(Ingredient)
-admin.site.register(Recipe)
 admin.site.register(RecipeIngredients)
 admin.site.register(FavoriteRecipe)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    search_help_text = 'Поиск по названию'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'author__username')
+    search_help_text = 'Поиск по названию и автору'
+
+    def in_favorites(self, obj):
+        return FavoriteRecipe.objects.filter(recipe=obj).count()
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Follow
+from .models import Follow, User
 
-admin.site.register(User)
+
 admin.site.register(Follow)
+
+
+@admin.register(User)
+class UsersAdmin(UserAdmin):
+    search_fields = ('username', 'email')
+    search_help_text = 'Поиск по автору и электронной почте'
