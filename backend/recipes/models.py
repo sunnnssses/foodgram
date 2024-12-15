a668a2b368cfa47936df83cd8f0525c16ae07ca9
Elizaveta <me@sunnsses.ru>
2024-12-15 18:32:38 +0300

    bugs fixed

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,5 @@
 import random
 
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
